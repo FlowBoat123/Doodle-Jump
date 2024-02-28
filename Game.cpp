cf82c77e6c9a7df1ae5372276e7c096f41efea55
FlowBoat123 <hienctcom@gmail.com>
2024-02-28 21:33:47 +0700

    transfer code from Game.cpp to level.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,16 +1,15 @@
 #include "Game.hpp"
-#include "doodle.hpp"
 #include "real.hpp"
-#include "Brickform.hpp"
-#include "Monsterform.hpp"
-#include "Score.hpp"
-#include "Platform.hpp"
+#include "Level.hpp"
 
 background *bg = nullptr;
-Doodle *mc = nullptr;
-Brick *br = nullptr;
-Score_bar *score = nullptr;
-Platform *Plat = nullptr;
+//Doodle *mc = nullptr;
+//Brick *br = nullptr;
+//Score_bar *score = nullptr;
+//Platform *Plat = nullptr;
+//Monster *monster = nullptr;
+//MonsterGroup *monsters = nullptr;
+Level *level = nullptr;
 int border = 200;
 
 Game::Game()
@@ -35,17 +34,23 @@ bool Game::init(const char *title, int xpos,int ypos,int width, int height, bool
         if( TTF_Init() == -1 ){ std::cout << "cant load ttf!\n"; }//Load ttf
 
         bg = new background();   bg->LoadImage(renderer,"background.png");
-        mc = new Doodle();       mc->LoadImage(renderer,"doodle.png");
-
-        score = new Score_bar();score->LoadImage(renderer,"top-score.png");score->LoadText(renderer,"Score : 0",{0,0,0});
-        Plat = new Platform();
-
-        for(int i = 0;i < 12;i++){
-            br = new Brick();        br->LoadImage(renderer,"brick.png");
-            br->setPosition(rand() % (width - 70),rand() % (height - 100));
-//            br->setPosition(200,400);
-            Plat->add(br);
-        }
+        level = new Level();
+        level->init(renderer);
+        level->LevelFromFile("Level1.txt");
+        level->LoadLevel(renderer);
+//        mc = new Doodle();       mc->LoadImage(renderer,"doodle.png");
+//
+//        score = new Score_bar();score->LoadImage(renderer,"top-score.png");score->LoadText(renderer,"Score : 0",{0,0,0});
+//        Plat = new Platform();
+//
+//        br = new Brick(); br->LoadImage(renderer,"brick.png");
+//        br->setPosition(300,400,0);
+//        Plat->add(br);
+//
+//        monsters = new MonsterGroup();
+//        monster = new Monster(); monster->LoadImage(renderer,"FlyMonster.png");
+//        monster->Set(100,430);
+//        monsters->add(monster);
         isRunning = true;
     }else{
         isRunning = false;
@@ -64,33 +69,38 @@ void Game::handleEvents()
         default:
             break;
     }
-
-    mc->handleEvent(event);
+    level->HandleEvent(event);
+//    mc->handleEvent(event);
 }
 bool collided;
 void Game::update()
 {
+    level->Update(renderer);
     //Doodle_Width = 58,Height = 57 | Doodle and brick inform | Brick_width = 70,height = 20;
-    collided = 0;
-    if(mc->getY() < border){//check if doodle cross the border
-        score->PassBorder();
-        Plat->update(mc->vY(),renderer);
-        if(mc->vY() < 0){
-            score->Plus(abs(mc->vY())); // update score
-        }
-        mc->scroll(border); // update pos doodle
-    }
-    else Plat->animation();
-    Plat->auto_update();
-
-    if(!score->isPassed() && mc->vY()<0){//change and update score
-        score->Plus(abs(mc->vY()));
-    }
-    score->changeScore(score->getPoint());
-    score->LoadText(renderer,score->getString(),{0,0,0});
-
-    if(Plat->CheckCollided( mc->getX() , mc->getY() , mc->getDoodleSprite() , mc->vY() ))collided = 1; // update platform and collided
-    mc->update(collided);
+//    collided = 0;
+//    if(mc->getY() < border){//check if doodle cross the border
+//        score->PassBorder();
+//        Plat->update(mc->vY(),renderer);
+//        if(mc->vY() < 0){
+//            score->Plus(abs(mc->vY())); // update score
+//        }
+//        mc->scroll(border); // update pos doodle
+//    }
+//    else Plat->animation();
+//    Plat->auto_update();
+//
+//    monsters->animation();
+//
+//
+//    if(!score->isPassed() && mc->vY()<0){//change and update score
+//        score->Plus(abs(mc->vY()));
+//    }
+//    score->changeScore(score->getPoint());
+//    score->LoadText(renderer,score->getString(),{0,0,0});
+//
+//    if(Plat->CheckCollided( mc->getX() , mc->getY() , mc->getDoodleSprite() , mc->vY() ))collided = 1; // check doodle and plat collided
+//    if(monsters->CheckCollided( mc->getX() , mc->getY() , mc->vY() ))collided = 1; // check doodle and monster collided
+//    mc->update(collided);
 }
 
 Game& Game::getInstance(){
@@ -102,11 +112,12 @@ void Game::render()
 {
     SDL_RenderClear(renderer);
     bg->render(renderer,0,0);
-    Plat->draw(renderer);
-
-    mc->render(renderer,mc->getX(),mc->getY(),mc->getSprite(mc->getDoodleSprite()));
-    score->renderBar(renderer,0,0);
-    score->renderPoint(renderer,4,0);
+    level->Render(renderer);
+//    Plat->draw(renderer);
+//    monsters->draw(renderer); // test monster
+//    mc->render(renderer,mc->getX(),mc->getY(),mc->getSprite(mc->getDoodleSprite()));
+//    score->renderBar(renderer,0,0);
+//    score->renderPoint(renderer,4,0);
 
     SDL_RenderPresent(renderer);
 }
--- Game.hpp
+++ Game.hpp
@@ -7,6 +7,7 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
 
+
 class Game{
 
     private:
