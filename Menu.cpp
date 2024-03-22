3b85cb23602e1d363cf914d77c1e6d2bb0f8ebf3
FlowBoat123 <hienctcom@gmail.com>
2024-03-22 21:56:58 +0700

    add gameover menu

--- Menu.cpp
+++ Menu.cpp
@@ -4,32 +4,37 @@ Menu::Menu()
 {
     mc = new Doodle();
     mon = new Monster();
-
-    clip_ufo[0] = {165,0,100,45};
-    clip_ufo[1] = {266,0,100,154};
+    ufo = new UFO();
+    button_play = new Button();
+    button_quit = new Button();
 
     clip_mc = {0,0,58,57};
 }
-SDL_Rect *Menu::getRect()
-{
-    Quad_ufo = {x,y,clip_ufo[curr_frame].w,clip_ufo[curr_frame].h};
-    return &Quad_ufo;
-}
+
 void Menu::LoadImage(SDL_Renderer *renderer)
 {
     free();
     bgTexture = IMG_LoadTexture(renderer,str_bg.c_str());
-    ufoTexture = IMG_LoadTexture(renderer,str_ufo.c_str());
     mcTexture = IMG_LoadTexture(renderer,str_mc.c_str());
 }
 
 void Menu::init(SDL_Renderer *renderer)
 {
     mc->SetPosition(48,200);
+    mc->LoadImage(renderer,str_mc.c_str());
+
     mon->SetType(2,renderer);
     mon->SetPosition(50,130);
-    x = 300;y = 45;
-    lastUpdate = 0.0;updateFrame = 0.0;
+
+    ufo->setPosition(300,45);
+    ufo->LoadImage(renderer);
+
+    button_play->LoadImage(renderer);
+    button_quit->LoadImage(renderer);
+
+    button_quit->SetPosition(250,450);
+    button_quit->SetType(1);
+    LoadImage(renderer);
 }
 
 void Menu::update(SDL_Renderer *renderer)
@@ -37,69 +42,14 @@ void Menu::update(SDL_Renderer *renderer)
     if(mc->getY() > 450)mc->update(1,0);
     else mc->update(0,0);
     mon->animation();
-    animation(renderer);
+    ufo->animation();
 }
 
-
-void Menu::animation(SDL_Renderer *renderer)
+void Menu::handleEvent(SDL_Event *e)
 {
-    current = SDL_GetTicks();
-    if(current - lastUpdate >= 2000.0 && longg){
-        longg = 0;
-        lastUpdate = current;
-        cnt++;
-    }
-    if(current - lastUpdate >= 1000.0 && !longg){
-        cnt++;
-        lastUpdate = current;
-        if(cnt == 1)longg = 1;
-    }
-    if(cnt == 3){
-        longg = 0;
-        if(direct)direct = 0;
-        else direct = 1;
-        cnt = 0;
-    }
-    if(direct){
-        if(!longg){
-            x -= 0.25;
-            y -= 0.25;
-        }
-        else{
-            x -= 0.25;
-            y += 0.25;
-        }
-    }
-    else{
-        if(!longg){
-            x += 0.25;
-            y -= 0.25;
-        }
-        else{
-            x += 0.25;
-            y += 0.25;
-        }
-    }
-    if(current - updateFrame >= 1200.0){
-        changeFrame = true;
-        updateFrame = current;
-        curr_frame = 1;
-        double_flash = rand() % 2;
-    }
-    if(changeFrame){
-        if(double_flash && current - updateFrame >= 79.0){
-            double_flash = 0;
-            curr_frame = 0;
-            updateFrame = current;
-        }
-        else if(double_flash == 0 && curr_frame == 0 && current - updateFrame >= 30.0){
-            curr_frame = 1;
-            updateFrame = current;
-        }
-        else if(current - updateFrame >= 80.0){
-            changeFrame = 0;
-            curr_frame = 0;
-        }
+    if(e->type == SDL_MOUSEBUTTONDOWN){
+        if(button_play->handleEvent(e))play = 1;
+        if(button_quit->handleEvent(e))quit = 1;
     }
 }
 
@@ -111,8 +61,9 @@ void Menu::Render(SDL_Renderer *renderer)
     SDL_Rect Quad_mc = {mc->getX(),mc->getY(),58,57};
     SDL_RenderCopy(renderer,mcTexture,&clip_mc,&Quad_mc);
 
-    SDL_RenderCopy(renderer,ufoTexture,&clip_ufo[curr_frame],getRect());
-
+    ufo->Render(renderer);
+    button_play->Render(renderer);
+    button_quit->Render(renderer);
     mon->render(renderer,mon->getX(),mon->getY(),mon->getSprite(2));
 }
 
@@ -120,6 +71,96 @@ void Menu::free()
 {
     if(bgTexture != nullptr)SDL_DestroyTexture(bgTexture);
     if(mcTexture != nullptr)SDL_DestroyTexture(mcTexture);
-    if(monTexture != nullptr)SDL_DestroyTexture(monTexture);
-    if(ufoTexture != nullptr)SDL_DestroyTexture(ufoTexture);
 }
+
+Pause::Pause()
+{
+    ufo = new UFO();
+    button_resume = new Button();
+}
+
+void Pause::init(SDL_Renderer *renderer)
+{
+    ufo->LoadImage(renderer);
+    ufo->setPosition(220,100);
+
+    button_resume->LoadImage(renderer);
+    button_resume->SetType(2);
+    button_resume->SetPosition(200,200);
+    LoadImage(renderer);
+}
+
+void Pause::LoadImage(SDL_Renderer *renderer)
+{
+    bgTexture = IMG_LoadTexture(renderer,str_bg.c_str());
+}
+
+void Pause::update(SDL_Renderer *renderer)
+{
+    ufo->animation();
+}
+
+void Pause::handleEvent(SDL_Event *e)
+{
+    if(e->type == SDL_MOUSEBUTTONDOWN)
+        if(button_resume->handleEvent(e))
+            resume = 1;
+    if(e->type == SDL_KEYDOWN)
+        if(e->key.keysym.sym == SDLK_c)resume = 1;
+}
+
+void Pause::Render(SDL_Renderer *renderer)
+{
+    SDL_Rect Quad_bg = {0,0,400,700};
+    SDL_RenderCopy(renderer,bgTexture,NULL,&Quad_bg);
+
+    ufo->Render(renderer);
+    button_resume->Render(renderer);
+}
+
+gameOver::gameOver()
+{
+    button_again = new Button();
+}
+
+void gameOver::LoadImage(SDL_Renderer *renderer)
+{
+    overBG_Texture = IMG_LoadTexture(renderer,str_over_bg.c_str());
+}
+
+void gameOver::init(SDL_Renderer *renderer)
+{
+    button_again->SetPosition(200,pY_again);
+    button_again->SetType(4);
+    button_again->LoadImage(renderer);
+    LoadImage(renderer);
+}
+
+void gameOver::update()
+{
+    if(posY == 0)return;
+    posY -= 10;
+    pY_again -= 10;
+    button_again->SetPosition(200,pY_again);
+}
+
+void gameOver::handleEvent(SDL_Event *e)
+{
+    if(e->type == SDL_MOUSEBUTTONDOWN){
+        if(button_again->handleEvent(e))again = 1;
+    }
+}
+
+void gameOver::Render(SDL_Renderer *renderer)
+{
+    SDL_Rect renderQuad = {posX,posY,400,700};
+    SDL_RenderCopy(renderer,overBG_Texture,NULL,&renderQuad);
+
+    button_again->Render(renderer);
+}
+
+void gameOver::free()
+{
+
+}
+
--- Menu.hpp
+++ Menu.hpp
@@ -4,6 +4,8 @@
 #include "Game.hpp"
 #include "doodle.hpp"
 #include "Monsterform.hpp"
+#include "Button.hpp"
+#include "UFO.hpp"
 
 class Menu{
 public:
@@ -12,32 +14,80 @@ public:
     void Render(SDL_Renderer *renderer);
     void init(SDL_Renderer *renderer);
     void update(SDL_Renderer *renderer);
-    void animation(SDL_Renderer *renderer);
+    void handleEvent(SDL_Event *e);
     void free();
 
-    SDL_Rect *getRect();
-
+    bool isQuit(){return quit;};
+    bool isPlay(){return play;};
 private:
     SDL_Texture *bgTexture = nullptr;
     SDL_Texture *mcTexture = nullptr;
-    SDL_Texture *ufoTexture = nullptr;
-    SDL_Texture *monTexture = nullptr;
 
     std::string str_bg = "covers/menu-cover.png";
     std::string str_mc = "doodle.png";
-    std::string str_ufo = "template/game-tiles.png";
 
     SDL_Rect clip_mc;
-    SDL_Rect clip_ufo[2];
-    SDL_Rect Quad_ufo;
 
-    Doodle *mc;
-    Monster *mon;
+    UFO *ufo = nullptr;
+    Doodle *mc = nullptr;
+    Monster *mon = nullptr;
+    Button *button_play = nullptr,*button_quit = nullptr;
 
-    double x = 0,y = 0;
-    int direct = 1,cnt = 0,curr_frame = 0,double_flash = 0;
-    bool longg = 0,up = 0,changeFrame = 0;
-    Uint32 current,ncurrent,lastUpdate,updateFrame;
+    bool quit = false,play = false;
+};
+
+class Pause{
+public:
+    Pause();
+    void init(SDL_Renderer *renderer);
+    void LoadImage(SDL_Renderer *renderer);
+    void Render(SDL_Renderer *renderer);
+    void handleEvent(SDL_Event *e);
+    void update(SDL_Renderer *renderer);
+    void SetPause(){resume = 0;}
+    bool isContinue(){return resume;};
+
+    void free();
+private:
+    SDL_Texture *bgTexture = nullptr;
+    std::string str_bg = "covers/pause-bg.png";
+
+    UFO *ufo = nullptr;
+    Button *button_resume = nullptr;
+
+    bool resume = 0;
+};
+
+class gameOver{
+public:
+    gameOver();
+    void LoadImage(SDL_Renderer *renderer);
+    void Render(SDL_Renderer *renderer);
+    void init(SDL_Renderer *renderer);
+    void handleEvent(SDL_Event *e);
+    void update();
+    bool isAgain(){return again;};
+    void setAgain(){
+        again = 0;
+        posY = 700;
+        pY_again = 700 + 400;
+        button_again->SetPosition(0,700+400);
+    };
+    void free();
+
+private:
+    bool again = 0;
+    SDL_Texture *overBG_Texture = nullptr;
+
+    int posX = 0,posY = 700;
+    int pY_again = 700+400;
+
+    std::string str_over_bg = "covers/gameover-cover.png";
+    std::string str_nor_bg = "covers/background.png";
+
+    Button *button_again = nullptr;
+
+
 };
 
 #endif // Menu_hpp
